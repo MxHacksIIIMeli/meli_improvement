c171ca10e6b53e9288a3fdf797c3f28be09ae135
Oscar Roa <oshardy3@gmail.com>
2015-10-03 23:31:38 -0500

    Se obtienen los datos de la busqueda

--- landing/forms.py
+++ landing/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class Search(forms.Form):
-	your_search = forms.CharField(label='Busca un producto', max_length=250)
+	q = forms.CharField(label='Busca un producto', max_length=250)
--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import Search
+import urllib2
 
 # Create your views here.
 def login(request):
@@ -8,4 +9,9 @@ def login(request):
 
 def index(request):
 	form = Search()
+	if request.method == 'GET':
+		q = request.GET.get('q', None)
+		url = "https://api.mercadolibre.com/sites/MLM/search?q={}".format(q.replace(' ', '%20'))
+		data = urllib2.urlopen(url)
+		print type(data)
 	return render(request, 'landing/index.html', {'form': form})
