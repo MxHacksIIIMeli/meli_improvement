a9ac512921076800a62c4c948b964f86c6fc5683
Oscar Roa <oshardy3@gmail.com>
2015-10-04 00:49:57 -0500

    Average prices for a query shown

--- landing/views.py
+++ landing/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import Search
 import urllib2
+import json
 
 # Create your views here.
 def login(request):
@@ -9,9 +10,17 @@ def login(request):
 
 def index(request):
 	form = Search()
+	prices = []
 	if request.method == 'GET':
 		q = request.GET.get('q', None)
 		url = "https://api.mercadolibre.com/sites/MLM/search?q={}".format(q.replace(' ', '%20'))
-		data = urllib2.urlopen(url)
-		print type(data)
+		print "*" * 20
+		print url
+		print "*" * 20
+		data = urllib2.urlopen(url).read()
+		resp = json.loads(str(data))
+		for i in range(len(resp['results'])):
+			prices.append(resp['results'][i]['price'])
+		average_price = sum(prices)/len(prices)
+		print average_price
 	return render(request, 'landing/index.html', {'form': form})
